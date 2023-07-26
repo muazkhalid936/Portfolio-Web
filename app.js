230bda38898720c32d23b285094e4e9e2f20ce19
Muaz Khalid <itzmajo786@gmail.com>
2023-07-26 14:32:30 +0500

    crated header of web

---
+++ app.js
@@ -0,0 +1,8 @@
+var typed = new Typed('#course', {
+    strings: ['C++ Programs', '', 'Websites'],
+    smartBackspace: true, // Default value
+    typeSpeed: 150,
+    backSpeed: 100,
+    loop: true
+  });
+  
